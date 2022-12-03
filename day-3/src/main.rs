7fbb44cac6960e5f88f14767957be07819c8fcea
wadafacc <schlumpftimo14@gmail.com>
2022-12-03 14:42:28 +0100

    do did done day 3 part 2

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -13,9 +13,40 @@ fn main() {
     let reader = BufReader::new(file);
 
     let mut sum = 0;
+    let mut lines: Vec<String> = Vec::new();
 
     for (index, line) in reader.lines().enumerate() {
         let line = line.unwrap();
+        lines.push(line);
+    }
+
+    for l in (0..lines.len()).step_by(3) {
+        let mut x: Vec<&str> = lines[l].split("").collect();
+        let mut y: Vec<&str> = lines[l + 1].split("").collect();
+        let mut z: Vec<&str> = lines[l + 2].split("").collect();
+        x.remove(x.len() - 1 as usize);
+        y.remove(y.len() - 1 as usize);
+        z.remove(z.len() - 1 as usize);
+        x.remove(0);
+        y.remove(0);
+        z.remove(0);
+
+        for item in &x {
+            if y.contains(item) {
+                if z.contains(item) {
+                    let i = item.chars().next().unwrap();
+                    let index = alphabet.iter().position(|&r| r == i).unwrap();
+                    sum += index + 1;
+                    break;
+                }
+            }
+        }
+    }
+
+    println!("{:?}", sum);
+}
+
+/*
 
         //half the string
         let mut x: Vec<&str> = line[(line.len() / 2)..].split("").collect();
@@ -25,14 +56,15 @@ fn main() {
         x.remove(0);
         y.remove(0);
 
+        println!("{:?}", x);
+        println!("{:?}", y);
+
         for item in &x {
             if y.contains(item) {
                 let i = item.chars().next().unwrap();
                 let index = alphabet.iter().position(|&r| r == i).unwrap();
                 sum += index + 1;
-                break;
             }
         }
-    }
-    println!("{:?}", sum);
-}
+
+*/
