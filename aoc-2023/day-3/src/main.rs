c05204252f42c5ef2a29893639af4e745c67cc9d
wadafacc <schlumpftimo14@gmail.com>
2023-12-03 22:47:54 +0100

    eep

--- aoc-2023/day-3/src/main.rs
+++ aoc-2023/day-3/src/main.rs
@@ -5,74 +5,53 @@ use std::io::{BufReader, BufRead};
 fn main() {
     // Open the file in read-only mode (ignoring errors).
     let file = File::open("./data.txt").unwrap();
-    let lines:Vec<String> = BufReader::new(file).lines().map(|l|l.expect("cant read lad")).collect();
+    let lines:Vec<String> = add_padding(BufReader::new(file).lines().map(|l|l.expect("cant read lad")).collect());
 
-    let mut numbers:HashMap<(u32,u32), u32> = HashMap::new();
+
+    println!("{:?}", lines);
+
+    let mut numbers:Vec<String> = Vec::new();
+    let mut temp_num = "".to_string();
 
     lines.iter().enumerate().for_each(|(y,line)| {
         for (x,c) in line.chars().enumerate() {
-            if !c.is_numeric() && c != '.' {
-                numbers.extend(check_surrounding(lines.clone(),x,y));
+            
+            if c.is_numeric(){
+                temp_num += &c.to_string();
+            }
+
+            if c == '.' && temp_num != "" && check_surrounding(lines.clone(),x,y) {
+                numbers.push(temp_num.clone());
+                temp_num = "".to_string();
             }
         }   
     });
 
-    for e in numbers.clone() {
-        (e.0.0-1..=e.0.0+1).for_each(|num| {
-            if numbers.contains_key(&(num, e.0.1)) {
-                numbers.remove(&(num, e.0.1));
-            }
-        });
-        (e.0.1-1..=e.0.1+1).for_each(|num| {
-            if numbers.contains_key(&(e.0.0,num)) {
-                numbers.remove(&(e.0.0,num));
-            }
-        });
-    }
-
-    println!("{:?}", numbers);
 }
 
-fn check_surrounding(lines: Vec<String>, x:usize, y:usize) -> HashMap<(u32,u32), u32> {
-    let mut numbers:HashMap<(u32,u32), u32> = HashMap::new();
-    (y-1..=y+1).for_each(|i| {
-        (x-1..=x+1).for_each(|j| {
-            let line:Vec<char> = lines[i].chars().collect();
-            if line[j].is_numeric() {
-                numbers.insert((j as u32,i as u32), get_num(line,j));
+fn check_surrounding(lines: Vec<String>, x:usize, y:usize) -> bool {
+    let mut out: bool = false;
 
+    (y-1..=(x+1).max(lines.len()-1)).for_each(|i| {
+        (x-1..=(x+1).max(lines[0].len()-1)).for_each(|j| {
+            let line:Vec<char> = lines[i].chars().collect();
+            if !line[j].is_numeric() && line[j] != '.' {
+                out = true;
             }
         });
     });
-
-    return numbers;
+    return out;
 }
 
-fn get_num(line: Vec<char>,x:usize) -> u32{
-    let mut num:Vec<char> = Vec::new();
+fn add_padding(input: Vec<String>) -> Vec<String> {
+    let mut with_padding:Vec<String> = Vec::new();
 
-    num.push(line[x]);
-
-    for (_i,n) in line.clone().as_slice()[..x].iter().rev().enumerate() {
-        if n.is_numeric() {
-            num.reverse();
-            num.push(*n);
-            num.reverse();
-        }
-        if *n == '.' {
-            break;
-        }
+    with_padding.push(".".repeat(input[0].len()));
+    for row in input.clone() {
+        let temp = ".".to_string() + &row + ".";
+        with_padding.push(temp);
     }
+    with_padding.push(".".repeat(input[0].len()));
 
-    for (_i,n) in line.clone().as_slice()[x+1..].iter().enumerate() {
-        if n.is_numeric() {
-            num.push(*n);
-        }
-        if *n == '.' {
-            break;
-        }
-    }
-
-    let number:String = num.iter().collect();
-    return number.parse::<u32>().expect("no unwrap");
+    return with_padding;
 }
