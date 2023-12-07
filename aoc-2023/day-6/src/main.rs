3eae407ce5a076ebcd3e28f1da78cf68389ede19
wadafacc <schlumpftimo14@gmail.com>
2023-12-07 11:35:47 +0100

    done d6p2

--- aoc-2023/day-6/src/main.rs
+++ aoc-2023/day-6/src/main.rs
@@ -9,26 +9,16 @@ fn main() {
     let file = File::open("./data.txt").unwrap();
     let lines:Vec<String> = BufReader::new(file).lines().map(|l|l.expect("cant read lad")).collect();
     
-    let time:Vec<i32> = lines[0].split_once(":").unwrap().1.split_ascii_whitespace().map(|x| x.parse::<i32>().unwrap()).collect();
-    let records:Vec<i32> = lines[1].split_once(":").unwrap().1.split_ascii_whitespace().map(|x| x.parse::<i32>().unwrap()).collect();
+    let time:String = lines[0].split_once(":").unwrap().1.split_ascii_whitespace().collect();
+    let records:String = lines[1].split_once(":").unwrap().1.split_ascii_whitespace().collect();
 
-    let mut beat_record: Vec<i32> = Vec::new();
-
-    for i in 0..records.len() {
-        beat_record.push(test_combinations(records[i], time[i]));
-    }
-
-    let mut total = 1;
-    beat_record.iter().for_each(|n| total *= n );
-
-
-    println!("{:?} | TOTAL: {}", beat_record, total);
+    println!("{:?}", test_combinations(records.parse::<i128>().unwrap(), time.parse::<i128>().unwrap()));
 
     // more bench
     println!("Elapsed time: {:.2?}", bench.elapsed());
 }
 
-fn test_combinations(record:i32, time:i32) -> i32 {
+fn test_combinations(record:i128, time:i128) -> i32 {
     let mut count = 0;
 
     for button_held in 0..=time {
