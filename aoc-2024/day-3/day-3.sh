564a80d6e37dd58d9c1d22a55a190511ca8722d1
wadafacc <schlumpftimo14@gmail.com>
2024-12-03 20:29:19 +0100

    do did done day 3 p 2

--- aoc-2024/day-3/day-3.sh
+++ aoc-2024/day-3/day-3.sh
@@ -1,21 +1,30 @@
 #! /bin/bash
 
 val=0
+enabled=1
 
 while IFS="" read -r p || [ -n "$p" ]
 do
-  matches=$(echo "$p" | grep -Eo "mul\([0-9]+,[0-9]+\)")
+  matches=($(echo "$p" | grep -Eo "mul\(\d+,\d+\)|do\(\)|don\'t\(\)"))
 
-  nums=($(echo "$matches" | grep -Eo "[0-9]+"))
+  for m in "${matches[@]}";
+  do
+    if [ $m = "don't()" ]; then
+      enabled=0
+    fi
+    if [ $m = "do()" ]; then
+      enabled=1
+    fi
 
-  l=${#nums[@]}
-  for ((i = 0; i < $l -1; i += 2)); do
-    p=$((${nums[$i]} * ${nums[$i + 1]}))
-    # echo "${nums[$i]} ${nums[$i + 1]}: $p"
-    val=$((val + p))
+    if [[ $m = "mul("* && $enabled = 1 ]]; then
+      nums=($(echo "$m" | grep -Eo "[0-9]+"))
+      l=${#nums[@]}
+
+      for ((i = 0; i < $l -1; i += 2)); do
+        val=$((val + $((${nums[$i]} * ${nums[$i + 1]}))))
+      done
+    fi
   done
-  
-  
 done < inputs.txt
 
 echo $val
