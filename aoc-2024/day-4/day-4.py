1236e6e1c7530006cb4d397b5ddf580cf1e0992d
wadafacc <tf@aduna.ch>
2024-12-05 17:19:53 +0100

    fuck this shit

--- aoc-2024/day-4/day-4.py
+++ aoc-2024/day-4/day-4.py
@@ -20,6 +20,7 @@ with open('./inputs.txt') as f:
 print()
 
 for y in range(0,len(l)):
+  print("---------- ROW ", y, "-----------")
   for x in range(0, len(l[y])):
     try:
       # horizontal
@@ -29,6 +30,7 @@ for y in range(0,len(l)):
         print("MATCH H")
         count += 1
     except IndexError:
+      print("H ERR")
       pass
 
     try:
@@ -39,6 +41,7 @@ for y in range(0,len(l)):
         print("MATCH V")
         count += 1
     except IndexError:
+      print("V ERR")
       pass
 
     try:
@@ -49,16 +52,18 @@ for y in range(0,len(l)):
         print("MATCH D1")
         count += 1
     except IndexError:
+      print("D1 ERR")
       pass
     
     try:
-      d2 = [l[y][x], l[y+1][x-1], l[y+2][x-2], l[y+3][x-3]]
+      d2 = [l[y][x], l[y-1][x+1], l[y-2][x+2], l[y-3][x+3]]
       print("d2 ", d2)
       if match(d2):
         print("MATCH D2")
         count += 1
     except IndexError:
+      print("D2 ERR")
       pass
   
-    print()
+
 print(count)
